4d780883e237543fed7fd66bd94ca793e68c42e2
nghip <23DH112274@st.huflit.edu.vn>
2025-08-29 21:55:41 +0700

    api_create board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,8 @@
 //hello
 //I'm Developer
 import { slugify } from '~/utils/formater'
+import { boardModel } from '~/models/boardModel'
+import { GET_DB } from '~/config/mongodb'
 const createNew = async( reqBody ) => {
   try {
     const newBoard = {
@@ -9,12 +11,15 @@ const createNew = async( reqBody ) => {
       slug:slugify(reqBody.title)
     }
     //goi toi tang model de xu ly luu tru bang ghi newBoard vao trong db
-    //...
-
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log('CreatedBoard', createdBoard)
+    //
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
     //lam them cac logic xu ly logic khac voi collection khac tuy vao dac thu cua du an ....
     // send Email, gui thong bao khi tao board moi chang han
 
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
