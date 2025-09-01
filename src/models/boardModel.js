d27af1a2ea4ae40b69bce866a8cd26692e650131
nghip <23DH112274@st.huflit.edu.vn>
2025-09-01 22:13:15 +0700

    Aggregate

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,7 +1,11 @@
 import Joi from 'joi'
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from './validators'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { GET_DB } from '~/config/mongodb'
 import { ObjectId } from 'mongodb'
+import { BOARD_TYPES } from '~/utils/constants'
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+
 
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
@@ -11,6 +15,7 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   columnOrderIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
   ).default([]),
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
   createAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy:Joi.boolean().default(false)
@@ -41,10 +46,29 @@ const findOneById = async (id) => {
 //query tong hop aggregate de lay toan bo column va cards thuoc ve Board
 const getDetails = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(id)
-    })
-    return result
+    // const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+    //   _id: new ObjectId(id)
+    // })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).aggregate([
+      { $match: {
+        _id:new ObjectId(id),
+        _destroy: false
+      } },
+      { $lookup: {
+        from :columnModel.COLUMN_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'columns'
+      } },
+      { $lookup: {
+        from :cardModel.CARD_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'cards'
+      } }
+    ]).toArray()
+    console.log(result)
+    return result[0] || {}
   } catch (error) {
     throw new Error(error)
   }
